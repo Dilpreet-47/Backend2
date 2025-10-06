feabcb48d8a598afdced59dec3617f8062c3e889
fedora <fedora@192.168.1.12>
2025-10-06 18:36:17 +0530

    cloudinary setup

---
+++ src/utils/claudinary.js
@@ -0,0 +1,32 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_CLOUD_KEY,
+  api_secret: process.env.CLOUDINARY_CLOUD_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    console.log("File Succesfully Uploaded", response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
+
+// cloudinary.v2.uploader.upload(
+//   "",
+//   { public_id: "olympic flag" },
+//   function (error, result) {
+//     console.log(result);
+//   }
+// );
+
+export {uploadOnCloudinary}
