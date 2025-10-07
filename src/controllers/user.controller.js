8969402ead939145611ed73272f49aee507c0bcb
Dilpreet-47 <singh.dilpreet.flux@gmail.com>
2025-10-07 17:54:43 +0530

    debugging1

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -31,7 +31,7 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new apiError(400, "password is required");
   }
 
-  const existedUser = User.findOne({
+ const existedUser = await User.findOne({
     $or: [{ username }, { email }],
   });
 
