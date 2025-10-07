ab49363bc3658c0395cf94c6949443eaba165755
Dilpreet-47 <singh.dilpreet.flux@gmail.com>
2025-10-07 12:15:53 +0530

    writing controllers routes and  testing using postman

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
 // require("dotenv").config({ path: "./env" });
 import dotenv from "dotenv";
 import connectDB from "./db/db.js";
+import { app } from "./app.js";
 
 dotenv.config({ path: "./.env" });
 
-connectDB();
-then(() => {
+connectDB()
+  .then(() => {
     app.on("error", (error) => {
-    console.log("error");
-    throw error;
+      console.log("error", error.message);
+    });
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`server is running at port: ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Mongo DB connection Failed !!!", error);
   });
-  app.listen(process.env.PORT || 5000, () => {
-    console.log(`server is running at port: ${process.env.PORT}`);
-  });
-}).catch((error) => {
-  console.log("Mongo DB connection Failed !!!", error);
-});
 // import express from "express";
 // const app = express()(async () => {
 //   try {
--- src/utils/apiError.js
+++ src/utils/apiError.js
@@ -14,4 +14,4 @@ class apiError extends Error {
   }
 }
 
-export {apiError}
+export { apiError };
